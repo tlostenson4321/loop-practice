26b84c4b6e8ee70dc70cba69925deb90634fcac7
Student <student@RU-019.local>
2015-09-07 14:49:49 -0600

    blarg

--- loop.js
+++ loop.js
@@ -2,6 +2,12 @@
  // for (var i = 0; i < 501; i = i +100) {
  // 	console.log(i);
  // }
- for ( var second = 1; second < 65; second = second * 2) {
- 	console.log(second);
- }
+ // for ( var second = 1; second < 65; second = second * 2) {
+ // 	console.log(second);
+ // }
+
+// var third = [1, 1, 1, 2, 2, 2, 3, 3, 3];
+
+ for (var i =1; i < 4; (i = i) (i=i) (i=i)(i=i+1)){ 
+ 	console.log(i);
+ }\
