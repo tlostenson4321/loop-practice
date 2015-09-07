8f56648b7cb1b708f49e6a741d20a23b1289284f
Student <student@RU-019.local>
2015-09-07 13:38:03 -0600

    second done

--- loop.js
+++ loop.js
@@ -1,6 +1,7 @@
-// var i 
 
- for (var i = 0; i < 501; i = i +100) {
- 	console.log(i);
+ // for (var i = 0; i < 501; i = i +100) {
+ // 	console.log(i);
+ // }
+ for ( var second = 1; second < 65; second = second * 2) {
+ 	console.log(second);
  }
-var second
